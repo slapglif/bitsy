6df11b445fd1b51bf6df4b0db3176954c610ea89
kuro112 <kuro@xtcr.net>
2015-11-21 08:58:59 -0700

    should be finished

--- bitsy.py
+++ bitsy.py
@@ -24,4 +24,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,threaded=True,host="204.12.211.219",post=80)
+    app.run(debug=True,threaded=True,host="204.12.211.219",port=80)
